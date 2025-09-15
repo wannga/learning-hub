d7c0f8fef762a58248de283bdfae48205776151c
wannga <wanwalaichen@gmail.com>
2025-09-16 02:23:17 +0700

    delete unused app folder

--- src/app/routes/routes.ts
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const router = express.Router();
-// import loginController from './controllers/LoginController';
-
-// router.post('http://localhost:3001/login', loginController.login);
-
-// module.exports = router;
